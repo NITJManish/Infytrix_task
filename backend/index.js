98439d7a1aeebcb3419b0a1499c60af4ed9efece
neera <kumarmanish150799@gmail.com>
2024-08-21 14:06:59 +0530

    partial implementation

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,18 @@ app.get("/",(req,res)=>{
 // routes
 app.use('/api',salesRoute);
 
+
+if (process.env.NODE_ENV === "PRODUCTION") {
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+    });
+  }
+  
+
+
+
 //using error middleware
 app.use(errorMiddleware);
 
